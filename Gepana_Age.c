e8f3122de2bb7a9b3a32325bfd8faa36c84f1e21
zsaffina <89230867+zsaffina@users.noreply.github.com>
2021-09-21 20:55:32 +0800

    Add files via upload

---
+++ Gepana_Age.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main ()
+{
+    //declaration of variable
+    int age = 18;
+    //printing of variable
+    printf("I am Zsaffina Pearl Cagatcagat Gepana, %d years of age", age);
+    return 0;
+}
