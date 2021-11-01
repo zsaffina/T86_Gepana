c37f8770443d674640f547868374e42970c49d55
zsaffina <89230867+zsaffina@users.noreply.github.com>
2021-11-02 00:12:47 +0800

    Add files via upload

---
+++ Gepana_Ma4FinalTaskPart II.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ()
+{
+    int n, ans;
+    printf("Input any positive integer: ");
+    scanf("%d", &n);
+
+    while(n !=1 ){
+        if(n % 2 == 1){
+            n = (n * 3) + 1;
+            printf("The current given value is %d\n", n);
+        } else {
+            n = n / 2;
+            printf("The current given value is %d\n", n);
+        }
+    }
+    return 0;
+}
